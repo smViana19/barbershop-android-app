77ea84b031fd2627c603319777b0038ff5279076
Samuu <samuelfilipevianadois@gmail.com>
2024-12-05 16:25:34 -0300

    feat: configure provides modules

---
+++ app/src/main/java/br/com/samuel/barbershopapplication/di/ApplicationModule.kt
@@ -0,0 +1,37 @@
+package br.com.samuel.barbershopapplication.di
+
+import br.com.samuel.barbershopapplication.backendservices.api.ApiService
+import br.com.samuel.barbershopapplication.constants.APP_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+@Module
+@InstallIn
+object ApplicationModule {
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .build()
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(APP_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+
+}
