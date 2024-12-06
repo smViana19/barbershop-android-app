cbf7da8a25d5909ec84c5a5caa9cb3eb26a08816
Samuu <samuelfilipevianadois@gmail.com>
2024-12-06 10:12:29 -0300

    feat: add generics composables components configs

---
+++ app/src/main/java/br/com/samuel/barbershopapplication/ui/components/AppTextField.kt
@@ -0,0 +1,16 @@
+package br.com.samuel.barbershopapplication.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AppTextField() {
+
+}
+
+@Preview
+@Composable
+private fun AppTextFieldPreview() {
+    
+}
