9e51d06acf5da60a5e83c77d57357901f2f5cd55
Samuu <samuelfilipevianadois@gmail.com>
2024-12-09 16:09:56 -0300

    feat: add assets app

--- app/src/main/java/br/com/samuel/barbershopapplication/ui/theme/Color.kt
+++ app/src/main/java/br/com/samuel/barbershopapplication/ui/theme/Color.kt
@@ -2,10 +2,31 @@ package br.com.samuel.barbershopapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+/**
+ * Color pallete
+ */
+val DodgerBlue = Color(0xFF1E90FF)
+val LightBlue = Color(0xFF63B8FF)
+val DarkBlue = Color(0xFF104E8B)
+
+
+/**
+ * Text colors
+ */
+
+val TextSecondary = Color(0xFFA8A8A8)
+/**
+ *  Color complementary
+ */
+
+val ComplementaryOrange = Color(0xFFFF8C1E)
+val Gold = Color(0xFFFFD700)
+
+/**
+ * Colors backgrounds
+ */
+val Background = Color(0xFFE0FFFF)
+val Surface = Color(0xFFF5F5F5)
+val TextDark = Color(0xFF000080)
+
--- app/src/main/java/br/com/samuel/barbershopapplication/ui/theme/Theme.kt
+++ app/src/main/java/br/com/samuel/barbershopapplication/ui/theme/Theme.kt
@@ -9,28 +9,27 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DodgerBlue,
+    secondary = LightBlue,
+    tertiary = DarkBlue
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = DodgerBlue,
+    secondary = LightBlue,
+    tertiary = DarkBlue,
+    background = Background,
+    surface = Surface,
     onPrimary = Color.White,
-    onSecondary = Color.White,
+    onSecondary = TextSecondary,
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
+
 )
 
 @Composable
