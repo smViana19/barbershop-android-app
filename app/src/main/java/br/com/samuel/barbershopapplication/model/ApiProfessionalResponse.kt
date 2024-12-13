789f6df9f460dda89cc339d4cccd2d6f96d55e17
Samuu <samuelfilipevianadois@gmail.com>
2024-12-13 18:46:42 -0300

    refactor: response retrofit mapper fixed

--- app/src/main/java/br/com/samuel/barbershopapplication/model/ApiProfessionalResponse.kt
+++ app/src/main/java/br/com/samuel/barbershopapplication/model/ApiProfessionalResponse.kt
@@ -3,10 +3,11 @@ package br.com.samuel.barbershopapplication.model
 import com.google.gson.annotations.SerializedName
 
 data class ApiProfessionalResponse (
-    @SerializedName("id")
-    val id: Int,
+
     @SerializedName("user")
     var user: ApiUserResponse,
+    @SerializedName("userId")
+    var userId: Int,
     @SerializedName("specialty")
     var specialty: ApiSpecialtyResponse
 )
