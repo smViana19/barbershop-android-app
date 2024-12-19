4c45b975a99708f8afdcaee4f91ce06fe078c8a2
Samuu <samuelfilipevianadois@gmail.com>
2024-12-18 22:54:09 -0300

    refactor: added new return type

--- app/src/main/java/br/com/samuel/barbershopapplication/backendservices/api/ApiAvailabilityService.kt
+++ app/src/main/java/br/com/samuel/barbershopapplication/backendservices/api/ApiAvailabilityService.kt
@@ -1,16 +1,16 @@
 package br.com.samuel.barbershopapplication.backendservices.api
 
 import br.com.samuel.barbershopapplication.model.ApiAvailabilityResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiAvailabilityService {
 
     @GET("/availabilities")
-    suspend fun getAllAvailabilites(): List<ApiAvailabilityResponse>
-
+    suspend fun getAllAvailabilites(): Response<List<ApiAvailabilityResponse>>
     @GET("/availabilities/{availabilityId}")
-    suspend fun getAvailabilityById(): List<ApiAvailabilityResponse>
+    suspend fun getAvailabilityById(@Path("availabilityId") availabilityId: Int): Response<List<ApiAvailabilityResponse>>
 
     @GET("/availabilities/professional/{professionalId}")
     suspend fun getAvailabilitiesByProfessionalId(@Path("professionalId") professionalId: Int): List<ApiAvailabilityResponse>
