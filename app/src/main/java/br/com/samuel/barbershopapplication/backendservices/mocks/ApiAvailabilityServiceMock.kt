1fd908911eb372bab401df16dd95b1d03056d4db
Samuu <samuelfilipevianadois@gmail.com>
2024-12-18 22:54:38 -0300

    feat: added availability mock class

---
+++ app/src/main/java/br/com/samuel/barbershopapplication/backendservices/mocks/ApiAvailabilityServiceMock.kt
@@ -0,0 +1,19 @@
+package br.com.samuel.barbershopapplication.backendservices.mocks
+
+import br.com.samuel.barbershopapplication.backendservices.api.ApiAvailabilityService
+import br.com.samuel.barbershopapplication.model.ApiAvailabilityResponse
+import retrofit2.Response
+
+class ApiAvailabilityServiceMock: ApiAvailabilityService {
+  override suspend fun getAllAvailabilites(): Response<List<ApiAvailabilityResponse>> {
+    TODO("Not yet implemented")
+  }
+
+  override suspend fun getAvailabilityById(availabilityId: Int): Response<List<ApiAvailabilityResponse>> {
+    TODO("Not yet implemented")
+  }
+
+  override suspend fun getAvailabilitiesByProfessionalId(professionalId: Int): List<ApiAvailabilityResponse> {
+    TODO("Not yet implemented")
+  }
+}
