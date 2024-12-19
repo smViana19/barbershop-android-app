68807241be1c7cac2becdce0489907808376b438
Samuu <samuelfilipevianadois@gmail.com>
2024-12-18 22:55:01 -0300

    feat: added id parameter to response data

--- app/src/main/java/br/com/samuel/barbershopapplication/model/ApiProfessionalResponse.kt
+++ app/src/main/java/br/com/samuel/barbershopapplication/model/ApiProfessionalResponse.kt
@@ -3,7 +3,8 @@ package br.com.samuel.barbershopapplication.model
 import com.google.gson.annotations.SerializedName
 
 data class ApiProfessionalResponse (
-
+    @SerializedName("id")
+    val id: Int,
     @SerializedName("user")
     var user: ApiUserResponse,
     @SerializedName("userId")
