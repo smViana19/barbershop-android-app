db4dd6c0dfa48786e6bfbcf8975826e4c65476ff
Samuu <samuelfilipevianadois@gmail.com>
2024-12-19 16:57:07 -0300

    feat: save token in shared preferences

--- app/src/main/java/br/com/samuel/barbershopapplication/ui/viewmodels/LoginViewModel.kt
+++ app/src/main/java/br/com/samuel/barbershopapplication/ui/viewmodels/LoginViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import br.com.samuel.barbershopapplication.backendservices.api.ApiAuthService
+import br.com.samuel.barbershopapplication.backendservices.sharedprefs.SharedPrefsService
 import br.com.samuel.barbershopapplication.model.ApiAuthRequest
 import br.com.samuel.barbershopapplication.ui.navigation.NavigationScreens
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,28 +15,31 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val apiAuthService: ApiAuthService,
-): ViewModel() {
-    private val _email = mutableStateOf("")
-    val email: MutableState<String> = _email
-    private val _password = mutableStateOf("")
-    val password: MutableState<String> = _password
+  private val apiAuthService: ApiAuthService,
+  private val sharedPrefsService: SharedPrefsService
+) : ViewModel() {
+  private val _email = mutableStateOf("")
+  val email: MutableState<String> = _email
+  private val _password = mutableStateOf("")
+  val password: MutableState<String> = _password
 
-    fun onClickButtonLogin(navController: NavController) {
-        viewModelScope.launch {
-            val apiAuthRequest = ApiAuthRequest(_email.value, _password.value)
-            try {
-                // TODO: Login request/response api 
-                val response = apiAuthService.login(apiAuthRequest)
-                if(response.isSuccessful) {
-                    navController.navigate(NavigationScreens.HOME_SCREEN.name)
-                } else {
-                    println("Erro ao logar")
-                }
-
-            } catch (e: Error) {
-                e.printStackTrace()
-            }
+  fun onClickButtonLogin(navController: NavController) {
+    viewModelScope.launch {
+      val apiAuthRequest = ApiAuthRequest(_email.value, _password.value)
+      try {
+        val response = apiAuthService.login(apiAuthRequest)
+        if (response.isSuccessful) {
+          val token = response.body()?.token
+          if (token != null) {
+            sharedPrefsService.saveAuthToken(token)
+          }
+          navController.navigate(NavigationScreens.HOME_SCREEN.name)
+        } else {
+          println("Erro ao logar")
         }
+      } catch (e: Error) {
+        e.printStackTrace()
+      }
     }
+  }
 }
