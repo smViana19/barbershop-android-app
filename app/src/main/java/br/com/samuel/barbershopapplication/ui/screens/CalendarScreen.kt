60751fbeb5203585f045193ea2561777ac9a9bd3
Samuu <samuelfilipevianadois@gmail.com>
2024-12-26 22:00:05 -0300

    refactor: improved calendar month

--- app/src/main/java/br/com/samuel/barbershopapplication/ui/screens/CalendarScreen.kt
+++ app/src/main/java/br/com/samuel/barbershopapplication/ui/screens/CalendarScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,6 +61,7 @@ fun CalendarScreen() {
   ) {
     Row(
       modifier = Modifier
+        .statusBarsPadding()
         .fillMaxWidth()
     ) {
       IconButton(
