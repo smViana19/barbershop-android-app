c15c666c95c82fc3bd46eb2ebf74006cd0f60a47
Samuu <samuelfilipevianadois@gmail.com>
2024-12-27 14:15:11 -0300

    feat: created appointment screen and viewmodel

---
+++ app/src/main/java/br/com/samuel/barbershopapplication/ui/screens/AppointmentScreen.kt
@@ -0,0 +1,30 @@
+package br.com.samuel.barbershopapplication.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.samuel.barbershopapplication.ui.theme.BarbershopApplicationTheme
+
+@Composable
+fun AppointmentScreen() {
+  Column(
+    modifier = Modifier.fillMaxSize()
+  ) {
+    Row {
+      Text(text = "Agendamentos")
+    }
+
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AppointmentScreenPreview() {
+  BarbershopApplicationTheme {
+    AppointmentScreen()
+  }
+}
---
+++ app/src/main/java/br/com/samuel/barbershopapplication/ui/viewmodels/AppointmentViewModel.kt
@@ -0,0 +1,12 @@
+package br.com.samuel.barbershopapplication.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import br.com.samuel.barbershopapplication.model.ApiAppointmentResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AppointmentViewModel @Inject constructor(
+  private val apiAppointmentResponse: ApiAppointmentResponse
+): ViewModel() {
+}
