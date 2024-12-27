4b9deab3171f0030c795be7d57a2ca63c3193607
Samuu <samuelfilipevianadois@gmail.com>
2024-12-27 17:52:12 -0300

    feat: added custom dialog

---
+++ app/src/main/java/br/com/samuel/barbershopapplication/ui/dialogs/AppDialog.kt
@@ -0,0 +1,9 @@
+package br.com.samuel.barbershopapplication.ui.dialogs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CustomDialog(modifier: Modifier = Modifier) {
+  
+}
