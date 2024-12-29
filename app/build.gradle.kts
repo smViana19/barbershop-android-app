ae73435f9355cd1317e56b76e21d7435d1d88ae2
Samuu <samuelfilipevianadois@gmail.com>
2024-12-28 21:09:40 -0300

    feat: added lottie animations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.hilt.common)
+    implementation(libs.lottie.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
